330adcc63ca6cb2104977c129fe271400fe71187
IslamEssam01 <essamislam260105@gmail.com>
2026-07-08 21:07:14 +0300

    chore: add DBSession type

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -17,3 +20,6 @@ class Base(DeclarativeBase):
 async def get_db():
     async with AsyncLocalSession() as session:
         yield session
+
+
+DBSession = Annotated[AsyncSession, Depends(get_db)]
