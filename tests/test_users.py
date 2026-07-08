25dfd559b224b3f262c5b3724715d8210563bb65
IslamEssam01 <essamislam260105@gmail.com>
2026-07-08 22:26:08 +0300

    test: add get user tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -94,3 +94,54 @@ async def test_create_duplicate_user(
 
     assert response.status_code == 409
     assert response.json()["detail"] == UserErrors.DUPLICATE_EMAIL
+
+
+@pytest.mark.anyio
+async def test_get_user_not_found(
+    client: AsyncClient,
+    db_session: AsyncSession,
+):
+    await db_session.execute(sql_delete(models.User))
+
+    response = await client.get("/api/users/9999")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == UserErrors.USER_NOT_FOUND
+
+
+@pytest.mark.anyio
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(
+    username=st.text(
+        min_size=1,
+        max_size=50,
+        alphabet=st.characters(exclude_categories=["Cs"], exclude_characters=["\x00"]),
+    ),
+    email=st.emails(),
+    password=st.text(
+        min_size=8,
+        max_size=200,
+        alphabet=st.characters(exclude_categories=["Cs"], exclude_characters=["\x00"]),
+    ),
+)
+async def test_get_user_successfully(
+    client: AsyncClient,
+    db_session: AsyncSession,
+    username: str,
+    email: str,
+    password: str,
+):
+    await db_session.execute(sql_delete(models.User))
+
+    user = await create_test_user(client, username, email, password)
+
+    response = await client.get(f"/api/users/{user["id"]}")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data.keys() == {"id", "username"}
+
+    assert data["id"] == user["id"]
+    assert data["username"] == user["username"]
--- utils/error_messages.py
+++ utils/error_messages.py
@@ -1,3 +1,4 @@
 class UserErrors:
     DUPLICATE_USERNAME = "A user with this username already exists"
     DUPLICATE_EMAIL = "A user with this email already exists"
+    USER_NOT_FOUND = "User not found"
