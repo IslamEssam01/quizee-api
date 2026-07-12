e565ad6eddcef363ab33efff43f3d89e4a5ad36e
IslamEssam01 <essamislam260105@gmail.com>
2026-07-12 17:38:29 +0300

    fix: users migration

--- alembic/versions/104bd433e907_users.py
+++ alembic/versions/104bd433e907_users.py
@@ -1,8 +1,8 @@
 """users
 
-Revision ID: 74cfedaa7251
+Revision ID: 104bd433e907
 Revises:
-Create Date: 2026-07-08 19:12:16.412862
+Create Date: 2026-07-12 17:34:20.508700
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "74cfedaa7251"
+revision: str = "104bd433e907"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -27,7 +27,7 @@ def upgrade() -> None:
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("username", sa.String(length=50), nullable=False),
         sa.Column("email", sa.String(length=200), nullable=False),
-        sa.Column("passwoed_hash", sa.String(length=200), nullable=False),
+        sa.Column("password_hash", sa.String(length=200), nullable=False),
         sa.Column(
             "created_at",
             sa.DateTime(timezone=True),
