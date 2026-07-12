a4b0319dd89a8b7236e2dff22ce216719882deb5
IslamEssam01 <essamislam260105@gmail.com>
2026-07-12 17:38:39 +0300

    feat: add CORS

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     db_url: str
     test_db_url: str
+    frontend_url: str
     secret_key: SecretStr
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from config import settings
 from routers import auth, users
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.frontend_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/api/users", tags=["Users"])
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
