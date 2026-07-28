45cf6d68de75a6ede638a0c9224827d0fcea977d
IslamEssam01 <essamislam260105@gmail.com>
2026-07-29 00:18:31 +0300

    feat: add dockerfile and health check

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 
 from config import settings
+from database import DBSession
 from routers import auth, quizzes, users
 
 app = FastAPI()
@@ -13,6 +15,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/health")
+async def health_check(db: DBSession):
+    try:
+        await db.execute(text("SELECT 1"))
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Database unavailable",
+        )
+
+    return {"status": "healthy"}
+
 
 app.include_router(users.router, prefix="/api/users", tags=["Users"])
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
